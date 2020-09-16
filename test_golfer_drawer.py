74c9018e618900f17c0b00c490db901f9d568d52
harrisonjones <harry.jones@loveholidays.com>
2020-09-16 17:57:49 +0100

    Fix golfer drawer test

--- test_golfer_drawer.py
+++ test_golfer_drawer.py
@@ -29,7 +29,14 @@ class TestGolferDrawer(unittest.TestCase):
 
 		power_line_length = golfer_drawer.GUIDE_LINE_LENGTH * 0.75
 		power_line_end_pos = (math.cos(self.golfer.direction) * power_line_length, math.sin(self.golfer.direction) * power_line_length)
-		mock_draw_line.assert_called_once_with(self.d_surf, golfer_drawer.DEFAULT_POWER_LINE_COLOUR, self.golfer.ball.pos, power_line_end_pos)
+		guide_line_end_pos = (math.cos(self.golfer.direction) * golfer_drawer.GUIDE_LINE_LENGTH, math.sin(self.golfer.direction) * golfer_drawer.GUIDE_LINE_LENGTH)
+
+		expected_line_calls = [
+		mock.call(self.d_surf, golfer_drawer.DEFAULT_GUIDE_LINE_COLOUR, self.golfer.ball.pos, guide_line_end_pos),
+		mock.call(self.d_surf, golfer_drawer.DEFAULT_POWER_LINE_COLOUR, self.golfer.ball.pos, power_line_end_pos)
+		]
+
+		self.assertEqual(expected_line_calls, mock_draw_line.call_args_list)
 
 	@mock.patch('pygame.draw.aaline')
 	def testDrawWithMovingBall(self, mock_draw_line):
